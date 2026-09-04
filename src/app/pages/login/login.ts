3559164092af78c616c5672482050373aa3a232f
DeathShadowBe <deathshadow1987@gmail.com>
2026-09-04 21:08:22 +0200

    Loginscreen updated

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,39 +1,69 @@
 import { Component } from '@angular/core';
-import { FormsModule,ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, FormsModule],
-  templateUrl: './login.html'
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
 })
 export class LoginComponent {
 
-  username = '';
-  password = '';
   error = '';
-  loginForm!: FormGroup;
+  loginForm: FormGroup;
 
-  constructor(
+  constructor(private fb: FormBuilder,
     private auth: AuthService,
     private router: Router
-  ) {}
+  ) {
+
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+  }
 
   login(): void {
 
-    const success = this.auth.login(
-      this.username,
-      this.password
-    );
-
-    if (success) {
-      this.router.navigate(['/transactions']);
-    } else {
-      this.error = 'Ongeldige login';
-    }
+  if (this.loginForm.invalid) {
+    return;
   }
+
+  const username = this.loginForm.value.username;
+  const password = this.loginForm.value.password;
+
+  const success = this.auth.login(
+    username,
+    password
+  );
+
+  if (success) {
+
+    this.router.navigate(['/transactions']);
+
+  } else {
+
+    this.error = 'Ongeldige login';
+
+  }
+
+}
 }
